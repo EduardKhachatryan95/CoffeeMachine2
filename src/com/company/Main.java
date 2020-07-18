2052207f28e62114f0961a10ec4842e8c45f9e2d
Eduard Khachatryan <eduardkhachatryan95@gmail.com>
2020-07-19 01:11:32 +0400

    added method minIng

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
+
         //in 1 cup of ingredients
         int milkCup = 50;
         int waterCup = 200;
@@ -21,49 +23,47 @@ public class Main {
         int water = waterSum / waterCup;
         int beans = beansSum / beansCup;
 
-        int min;
-        if (beans < milk && beans < water)
-        {
-            min = beans;
-        }
-        else if (milk < beans && milk < water)
-        {
-            min = milk;
-        }
-        else
-        {
-            min = water;
-        }
+        int min = minIng(milk, water, beans);
 
         System.out.println("Write how many cups of coffee you will need:");
         int cup;
 
-        while(min!=0)
-        {
+        while (min != 0) {
 
             cup = s.nextInt();
 
-            if (min >0 && min>cup)
-            {
+            if (min > 0 && min > cup) {
                 System.out.println("Yes, I can make that amount of coffee (and even " + (min - cup) + " more than that)");
                 min -= cup;
-            }
-            else if( min==cup)
-            {
+            } else if (min == cup) {
                 System.out.println("Yes, I can make that amount of coffee");
                 min -= cup;
-            }
-            else
-            {
+            } else {
                 System.out.print("No, I can make only " + min + " cup(s) of coffee\n");
             }
-            waterSum -= cup*waterCup;
-            milkSum -= cup*milkCup;
-            beansSum -= cup*beansCup;
+            waterSum -= cup * waterCup;
+            milkSum -= cup * milkCup;
+            beansSum -= cup * beansCup;
 
         }
 
 
-
     }
+
+    public static int minIng(int n1, int n2, int n3) {
+        int min;
+        if (n1 < n2 && n1 < n3) {
+            min = n1;
+        } else if (n2 < n1 && n2 < n3) {
+            min = n2;
+        } else {
+            min = n3;
+        }
+        return min;
+    }
+
+
 }
+
+
+
